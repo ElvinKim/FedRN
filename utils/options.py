c9219ad32e031bf8f8cbe465833dbb9d105b812e
wonyoung.shin <wonyoung.shin@navercorp.com>
2021-05-15 15:33:47 +0900

    Update default args

--- utils/options.py
+++ utils/options.py
@@ -16,10 +16,10 @@ def args_parser():
                         help='method name')
 
     # federated arguments
-    parser.add_argument('--epochs', type=int, default=10, help="rounds of training")
+    parser.add_argument('--epochs', type=int, default=500, help="rounds of training")
     parser.add_argument('--num_users', type=int, default=100, help="number of users: K")
     parser.add_argument('--frac', type=float, default=0.1, help="the fraction of clients: C")
-    parser.add_argument('--local_ep', type=int, default=2, help="the number of local epochs: E")
+    parser.add_argument('--local_ep', type=int, default=5, help="the number of local epochs: E")
     parser.add_argument('--local_bs', type=int, default=50, help="local batch size: B")
     parser.add_argument('--bs', type=int, default=128, help="test batch size")
     parser.add_argument('--lr', type=float, default=0.01, help="learning rate")
@@ -35,7 +35,7 @@ def args_parser():
                         help="federated learning method")
 
     # model arguments
-    parser.add_argument('--model', type=str, default='mlp', choices=['mlp', 'cnn', 'mobile', 'cnn4conv'],
+    parser.add_argument('--model', type=str, default='cnn4conv', choices=['mlp', 'cnn', 'mobile', 'cnn4conv'],
                         help='model name')
     parser.add_argument('--kernel_num', type=int, default=9, help='number of each kind of kernel')
     parser.add_argument('--kernel_sizes', type=str, default='3,4,5',
