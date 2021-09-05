30a809223bf42dbd6b782c09770ff45054969642
elvinkim <bonwho09@gmail.com>
2021-09-05 23:18:19 +0900

    remove unnecessary files

--- run_mnist_fed.sh
+++
@@ -1,5 +0,0 @@
-# IID
-python3 main_fed.py --dataset mnist --model cnn --epochs 50 --schedule 30 40 --num_channels 1 --save_dir mnist --iid
-
-# Non-IID
-python3 main_fed.py --dataset mnist --model cnn --epochs 50 --schedule 30 40 --num_channels 1 --save_dir mnist
