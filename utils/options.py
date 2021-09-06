003df533b2f6224fa8b6de30768f0bdf0c795d74
wonyoung.shin <wonyoung.shin@navercorp.com>
2021-09-06 15:40:07 +0900

    Remove unnecessary models

--- utils/options.py
+++ utils/options.py
@@ -34,16 +34,7 @@ def args_parser():
                         help="federated learning method")
 
     # model arguments
-    parser.add_argument('--model', type=str, default='cnn4conv', choices=['mlp', 'cnn', 'mobile', 'cnn4conv', 'cnn9'],
-                        help='model name')
-    parser.add_argument('--kernel_num', type=int, default=9, help='number of each kind of kernel')
-    parser.add_argument('--kernel_sizes', type=str, default='3,4,5',
-                        help='comma-separated kernel size to use for convolution')
-    parser.add_argument('--norm', type=str, default='batch_norm', help="batch_norm, layer_norm, or None")
-    parser.add_argument('--num_filters', type=int, default=32, help="number of filters for conv nets")
-    parser.add_argument('--max_pool', type=str, default='True',
-                        help="Whether use max pooling rather than strided convolutions")
-    parser.add_argument('--reproduce', action='store_true', help='reproduce paper code')
+    parser.add_argument('--model', type=str, default='cnn4conv', choices=['cnn4conv'], help='model name')
 
     # other arguments
     parser.add_argument('--dataset', type=str, default='mnist', help="name of dataset",
